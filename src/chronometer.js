f3b64805309777e10279c1fac3c03f38cca9ed92
bubu-destroi <zzzarolina@gmail.com>
2024-04-13 19:42:03 +0100

    lab solved, last jasmine spec with error

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,73 @@
 class Chronometer {
+  
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
     // ... your code goes here
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+  this.intervalId =  setInterval(() => {
+      if (printTimeCallback)  printTimeCallback()
+      this.currentTime +=1
+  
+  }, 1000)
+ 
   }
 
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime === 0){return 0}
+    let inMin = this.currentTime/60
+    let ffloor = Math.floor(inMin)
+    
+    return ffloor
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0){return 0}
+    let remain =  this.currentTime % 60
+
+    return remain
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    let pad = '0'
+    let newVal = value.toString()
+    if (newVal.length === 1)  {
+      pad += newVal
+    }
+    if (newVal.length === 2){
+      pad = newVal
+    }
+
+  return pad
   }
 
-  stop() {
+  stop() { 
+
+    clearInterval(this.intervalId)  
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime=0
     // ... your code goes here
   }
 
   split() {
+    let min = this.getMinutes(this.currentTime)
+    let mmin = this.computeTwoDigitNumber(min)
+
+    let sec = this.getSeconds(this.currentTime)
+    let ssec = this.computeTwoDigitNumber(sec)
+
+    result = `${mmin}:${ssec}`
+
+    return result
+
     // ... your code goes here
   }
 }
+
+
